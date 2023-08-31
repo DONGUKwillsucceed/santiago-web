81207d8eb4c2d5744a141338aed2b66f2125084e
DONGUKwillsucceed <rlaehddnr1009@naver.com>
2023-08-31 21:21:22 +0900

    main-ad-container 도입

---
+++ components/main-ad-container.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+interface Props {
+  imageUrl: string;
+}
+
+export default function MainAdContainer({ imageUrl }: Props) {
+  return (
+    <div className="w-4/5 px-8">
+      <Image
+        src={imageUrl}
+        alt={"main ad banner"}
+        layout="responsive"
+        width={1200}
+        height={300}
+        quality={100}
+      />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 import HeaderBar from "@/components/header-bar";
+import MainAdContainer from "@/components/main-ad-container";
 import { Roboto } from "next/font/google";
+import Image from "next/image";
 
 const roboto = Roboto({ weight: "300", subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <HeaderBar
-      needBackButton={false}
-      loginInfo={{
-        id: "abc",
-        name: "Andrew",
-        imageUrl: null,
-      }}
-    />
+    <div>
+      <HeaderBar
+        needBackButton={false}
+        loginInfo={{
+          id: "abc",
+          name: "Andrew",
+          imageUrl: null,
+        }}
+      />
+      <div className="flex justify-center">
+        <MainAdContainer imageUrl={"/main_ad_banner.png"}/>
+      </div>
+    </div>
   );
 }
