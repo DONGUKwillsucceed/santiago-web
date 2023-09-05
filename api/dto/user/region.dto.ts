37604273069fdecefa8109855d6e1ace3211cc32
DONGUKwillsucceed <rlaehddnr1009@naver.com>
2023-09-05 11:19:32 +0900

    regionSelector가 추가된 9개 언어로 구동이 가능하게 하였음

--- api/dto/user/region.dto.ts
+++ api/dto/user/region.dto.ts
@@ -1,7 +1,12 @@
 export interface RegionDto {
-    id: string;
-    name_en: string;
-    name_kr: string;
-    name_hk: string;
-    name_jp: string;
+    id: string,
+    name_en: string,
+    name_kr: string,
+    name_hk: string,
+    name_ch: string,
+    name_jp: string,
+    name_fr: string,
+    name_vi: string,
+    name_ge: string,
+    name_it: string,
 }
--- util/region-selector.ts
+++ util/region-selector.ts
@@ -2,8 +2,14 @@ import { RegionDto } from "@/api/dto/user/region.dto";
 
 export function regionSelector(region: RegionDto, locale: string) {
     if(locale === 'ko-KR') return region.name_kr;
-    else if(locale === 'ja_JP') return region.name_jp;
-    else if(locale === 'zh_HK') return region.name_hk;
-    else if(locale === 'en_HK') return region.name_en;
+    else if(locale === 'ja-JP') return region.name_jp;
+    else if(locale === 'zh-HK') return region.name_hk;
+    else if(locale === 'en-HK') return region.name_en;
+    else if(locale === 'zh-TW' || locale === 'zh-CN') return region.name_ch;
+    else if(locale === 'fr-FR') return region.name_fr;
+    else if(locale === 'it-IT') return region.name_ge;
+    else if(locale === 'vi-VN') return region.name_it;
+    else if(locale === 'de-DE') return region.name_vi;
+
     else return region.name_en;
 }
